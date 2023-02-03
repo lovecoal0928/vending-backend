800b7b4763aad3b614ee1d74e3353080fe36489c
coa3 <lovecoal928@gmail.com>
2023-02-03 15:26:32 +0900

    fix Imagefield to Charfield

--- vending/models.py
+++ vending/models.py
@@ -6,7 +6,7 @@ class Drinks(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField("ドリンク名", max_length=50)
     price = models.IntegerField("価格")
-    src = models.ImageField(upload_to="images", verbose_name="イメージ画像")
+    src = models.CharField(verbose_name="イメージ画像", max_length=100)
     hot_flag = models.BooleanField("温度フラグ", default=False)
     soda_flag = models.BooleanField("炭酸フラグ", default=False)
     created_at = models.DateTimeField(verbose_name="追加日時", auto_now_add=True)
